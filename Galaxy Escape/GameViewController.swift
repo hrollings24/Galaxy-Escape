0c98ed6f23dfcd3ce978425ad123941e4b8ba61f
Harry Rollings <harryrollings@icloud.com>
2019-11-21 11:03:33 +0000

    Added ability to spawn a square in the game scene

--- Galaxy Escape/GameViewController.swift
+++ Galaxy Escape/GameViewController.swift
@@ -7,117 +7,137 @@
 //
 
 import UIKit
-import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
 
+    var scnView: SCNView!
+    var scnScene: SCNScene!
+    var cameraNode: SCNNode!
+    
+    var timer = Timer()
+
+
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        setupView()
+        setupScene()
+        setupCamera()
+        addSpaceship()
+        scheduledTimerWithTimeInterval()
+
         
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
-        scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        // animate the 3d object
-        //ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
-        
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = true
-        
-        // configure the view
-        scnView.backgroundColor = UIColor.black
-        
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
     }
     
-    
-    
-    @objc
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result = hitResults[0]
-            
-            // get its material
-            let material = result.node.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
-        }
-    }
-    
+ 
     override var shouldAutorotate: Bool {
         return true
     }
-    
+
     override var prefersStatusBarHidden: Bool {
         return true
     }
     
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
+    func setupView() {
+        scnView = self.view as? SCNView
+        
+        scnView.autoenablesDefaultLighting = true
+    }
+    
+    func setupScene() {
+      scnScene = SCNScene()
+      scnView.scene = scnScene
+        
+        scnScene.background.contents = "GeometryFighter.scnassets/Textures/Comet Clash Background.png"
+    }
+    
+    func setupCamera() {
+      // 1
+      cameraNode = SCNNode()
+      // 2
+      cameraNode.camera = SCNCamera()
+      // 3
+      cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+      // 4
+      scnScene.rootNode.addChildNode(cameraNode)
+    }
+    
+    func spawnShape() {
+        // 1
+        var geometry:SCNGeometry
+        // 2
+        switch ShapeType.random() {
+        default:
+        // 3
+        geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0,
+          chamferRadius: 0.0)
         }
+        // 4
+        let geometryNode = SCNNode(geometry: geometry)
+        // 5
+        geometryNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        geometryNode.physicsBody?.isAffectedByGravity = false
+        // 1
+        let randomX = randRange(lower: -12, upper: -9)
+        let randomY = randRange(lower: -5, upper: 5)
+        // 2
+        let force = SCNVector3(x: randomX, y: randomY , z: 0)
+        // 3
+        let position = SCNVector3(x: 0.05, y: 0.05, z: 0.05)
+        // 4
+        geometryNode.physicsBody?.applyForce(force, at: position, asImpulse: true)
+
+        geometryNode.position = SCNVector3(x: 18, y: 0, z: 0)
+        scnScene.rootNode.addChildNode(geometryNode)
+        
+    }
+    
+    func addSpaceship(){
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let shipNode = scene.rootNode.childNode(withName: "ship", recursively: true)!
+
+        shipNode.position = SCNVector3(x: -10, y: 0, z: 0)
+        scnScene.rootNode.addChildNode(shipNode)
+
+        
+    }
+    
+    func scheduledTimerWithTimeInterval(){
+        // Scheduling timer to Call the function "spawnMeteor" with the interval of 1 seconds
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.spawnMeteor), userInfo: nil, repeats: true)
     }
 
+    @objc func spawnMeteor(){
+        spawnShape()
+    }
+    
+    func randRange (lower: Float , upper: Float) -> Float {
+        return lower + Float(arc4random_uniform(UInt32(upper - lower + 1)))
+    }
+    
+    
+}
+
+enum ShapeType:Int {
+
+  case box = 0
+  case sphere
+  case pyramid
+  case torus
+  case capsule
+  case cylinder
+  case cone
+  case tube
+
+  // 2
+  static func random() -> ShapeType {
+    let maxValue = tube.rawValue
+    let rand = arc4random_uniform(UInt32(maxValue+1))
+    return ShapeType(rawValue: Int(rand))!
+  }
+    
+    
 }
