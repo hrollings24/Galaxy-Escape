d3fc658129622c476b12fcb09a7775b68f02af89
Harry Rollings <harryrollings@icloud.com>
2020-02-07 13:55:37 +0000

    fixed issue #3

--- Galaxy Escape/LeaderboardViewController.swift
+++ Galaxy Escape/LeaderboardViewController.swift
@@ -11,8 +11,8 @@ import GameKit
 class LeaderboardViewController: UIViewController, GKGameCenterControllerDelegate {
 
     var gameVC: GameViewController!
+    var scoresArray = [UIView]()
 
-    
     @IBOutlet var friendSV: UIScrollView!
     @IBOutlet var worldSV: UIScrollView!
     
@@ -21,9 +21,6 @@ class LeaderboardViewController: UIViewController, GKGameCenterControllerDelegat
         self.dismiss(animated: true, completion: nil)
         gameVC.setupMenu()
     }
-    
-    var scoreArray = [UIView]()
-    var friendArray = [UIView]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,21 +48,21 @@ class LeaderboardViewController: UIViewController, GKGameCenterControllerDelegat
                 let nameLabel = UILabel()
                 nameLabel.text = score.player.alias
                 nameLabel.textColor = .white
-                nameLabel.frame.size = CGSize(width: self.worldSV.frame.width/2, height: 40)
+                nameLabel.frame.size = CGSize(width: self.worldSV.frame.width/3*2, height: 40)
                 nameLabel.adjustsFontSizeToFitWidth = true
                 nameLabel.numberOfLines = 0
-                nameLabel.font = nameLabel.font.withSize(48)
+                nameLabel.font = UIFont(name: "SpacePatrol", size: 48)
                 let scoreLabel = UILabel()
                 scoreLabel.text = String(score.value)
                 scoreLabel.adjustsFontSizeToFitWidth = true
                 scoreLabel.numberOfLines = 0
                 scoreLabel.textColor = .white
-                scoreLabel.font = scoreLabel.font.withSize(48)
+                scoreLabel.font = UIFont(name: "SpacePatrol", size: 48)
                 scoreLabel.textAlignment = .right
-                scoreLabel.frame.size = CGSize(width: self.worldSV.frame.width/2, height: 40)
+                scoreLabel.frame.size = CGSize(width: self.worldSV.frame.width/3, height: 40)
                 let newView = UIView()
                 newView.backgroundColor = UIColor.red
-                scoreLabel.frame.origin.x = self.worldSV.frame.width/2
+                scoreLabel.frame.origin.x = self.worldSV.frame.width/3*2
                 newView.sizeThatFits(CGSize(width: self.worldSV.frame.width, height: 40))
                 newView.addSubview(nameLabel)
                 //scoreLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -83,9 +80,10 @@ class LeaderboardViewController: UIViewController, GKGameCenterControllerDelegat
         }
     }
     
+    //runs in background thread
     func addToScreen(limit: GKLeaderboard.PlayerScope, array: [UIView]) {
         var yPosition: CGFloat = 0.0
-        
+                
         for view in array{
 
             view.frame.origin.x = 0
@@ -106,6 +104,29 @@ class LeaderboardViewController: UIViewController, GKGameCenterControllerDelegat
         else{
             self.friendSV.contentSize.height = yPosition
         }
+        
+        scoresArray += array
+        
+        self.checkFontSizes()
+        
+    }
+    
+    func checkFontSizes(){
+        var minimumSize = CGFloat(48)
+        for view in scoresArray{
+            let labels = view.subviews.compactMap { $0 as? UILabel }
+
+            for label in labels {
+                let fontSize = label.fontSize
+                if fontSize < minimumSize{
+                    minimumSize = fontSize
+                }
+            }
+            for label in labels {
+                print(minimumSize)
+                label.font = label.font.withSize(minimumSize)
+            }
+        }
     }
     
 
@@ -113,4 +134,33 @@ class LeaderboardViewController: UIViewController, GKGameCenterControllerDelegat
         gameCenterViewController.dismiss(animated: true, completion: nil)
     }
 
+}
+
+
+extension UILabel {
+
+    /// The receiver’s font size, including any adjustment made to fit to width. (read-only)
+    ///
+    /// If `adjustsFontSizeToFitWidth` is not `true`, this is just an alias for
+    /// `.font.pointSize`. If it is `true`, it returns the adjusted font size.
+    ///
+    /// Derived from: [http://stackoverflow.com/a/28285447/5191100](http://stackoverflow.com/a/28285447/5191100)
+    var fontSize: CGFloat {
+        get {
+            if adjustsFontSizeToFitWidth {
+                var currentFont: UIFont = font
+                let originalFontSize = currentFont.pointSize
+                var currentSize: CGSize = (text! as NSString).size(withAttributes: [NSAttributedString.Key.font: currentFont])
+
+                while currentSize.width > frame.size.width && currentFont.pointSize > (originalFontSize * minimumScaleFactor) {
+                    currentFont = currentFont.withSize(currentFont.pointSize - 1)
+                    currentSize = (text! as NSString).size(withAttributes: [NSAttributedString.Key.font: currentFont])
+                }
+
+                return currentFont.pointSize
+            }
+
+            return font.pointSize
+        }
+    }
 }
