36eb0e977bfa2080335ed6fdace8143f3b21d477
Harry Rollings <harryrollings@icloud.com>
2020-02-27 17:20:18 +0000

    fixed some bugs - speed increase now works - planets now spawn correctly after initial game setup

---
+++ Pods/Target Support Files/Pods-Galaxy Escape-Galaxy EscapeUITests/Pods-Galaxy Escape-Galaxy EscapeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Galaxy_Escape_Galaxy_EscapeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Galaxy_Escape_Galaxy_EscapeUITests
+@end
